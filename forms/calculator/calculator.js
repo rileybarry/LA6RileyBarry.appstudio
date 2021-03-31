3d4db7b8c27ef7269a69d610b73e8409e38966ce
rileybarry <60715866+rileybarry@users.noreply.github.com>
2021-03-30 19:35:24 -0500

    completed calculator

--- forms/calculator/calculator.js
+++ forms/calculator/calculator.js
@@ -1,12 +1,19 @@
 function mult(x, y) {
-return x * y
+  let numOne = Number(x)
+  let numTwo = Number(y)
+  return numOne * numTwo
 }
 
-function add(x1, x2) {
-  return x1 + y2
+function add(x1, y1) {
+  let num1 = Number(x1)
+  let num2 = Number(y1)
+  return num1 + num2
 }
-
 
 btnMult.onclick=function(){
   lblOut.value = mult(inpOne.value, inpTwo.value)
+}
+
+btnAdd.onclick=function(){
+  lblOut.value = add(inpOne.value, inpTwo.value)
 }
